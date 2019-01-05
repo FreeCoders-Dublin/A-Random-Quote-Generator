cb788624168dcce6a9c01012f6ccfd21eabcbf20
valeriosclip <valeriosclip3@gmail.com>
2019-01-05 12:32:40 +0000

    new comments js file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object.
-***/
+// Create the array of quote objects and name it `quotes`.
 
 var quotes = [
   {
@@ -51,12 +35,11 @@ var quotes = [
 ]
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number
-   - use the random number to `return` a random quote object from the
-     `quotes` array.
-***/
+
+//Created the `getRandomQuote` function to:
+// - generate a random number
+// - use the random number to `return` a random quote object from the`quotes` array.
+
 function getRandomQuote(list) {
     //generate a random number
    randomNumber = Math.floor(Math.random() * list.length);
@@ -67,15 +50,10 @@ function getRandomQuote(list) {
 
 
 
-/***
-  Create the `printQuote` function to:
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+
+//Create the `printQuote` function to:
+//   - call the `getRandomQuote` function and assign it to a variable.
+//  - use the properties of the quote object stored in the variable to create HTML string.
 
 function printQuote() {
   changeColorBg();
@@ -93,12 +71,6 @@ function printQuote() {
    document.getElementById('quote-box').innerHTML = contentHtml;
  }
 
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
 
 // it changes the color based on a random string decoded on 16.
 let changeColorBg = () => {
@@ -106,7 +78,9 @@ let changeColorBg = () => {
   document.body.style.backgroundColor = colorSelected;
 }
 
+// When the "Show another quote" button is clicked, the event listener
+// will call the `printQuote`function.
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 document.addEventListener("DOMContentLoaded", printQuote, false);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
